c8185560d3255f062d08d8360cd869cb0c8d5ffa
Irving78rus <isakov911@mail.ru>
2021-08-18 16:11:32 +0300

    Презентационная и Контейнерная компоненты

--- src/component/Header/NeighborsContainer.js
+++ src/component/Header/NeighborsContainer.js
@@ -1,40 +1,83 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Users from './MyNeighbors'
-import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from '../../redux/UsersReducer';
+import React from "react";
+import { connect } from "react-redux";
+import Users from "./MyNeighbors";
+import axios from "axios";
+import {
+    followAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unfollowAC,
+} from "../../redux/UsersReducer";
+
+class UsersContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        axios
+            .get(
+                `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.current}&count=${this.props.pageSize} `
+            )
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios
+            .get(
+                `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize} `
+            )
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+            });
+    };
+
+    render() {
+        return (
+            <Users
+                users={this.props.users}
+                pageSize={this.props.pageSize}
+                totalUsersCount={this.props.totalUsersCount}
+                currentPage={this.props.currentPage}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                onPageChanged={this.onPageChanged}
+            />
+        );
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage:state.usersPage.currentPage
-    }
-     
-}
+        currentPage: state.usersPage.currentPage,
+    };
+};
 let mapDispatchToProps = (dispatch) => {
     return {
-        follow: (userId) =>{
-            dispatch(followAC(userId))
+        follow: (userId) => {
+            dispatch(followAC(userId));
         },
-        unfollow: (userId) =>{
-            dispatch(unfollowAC(userId))
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId));
         },
-        setUsers: (users) =>{
-            dispatch(setUsersAC(users))
+        setUsers: (users) => {
+            dispatch(setUsersAC(users));
         },
-        setCurrentPage: (pageNumber) =>{
-            dispatch(setCurrentPageAC(pageNumber))
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber));
         },
-        setTotalUsersCount: (totalCount) =>{
-            dispatch(setTotalUsersCountAC(totalCount))
-        }
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount));
+        },
+    };
+};
 
-    }
-     
-}
-
-export default connect (mapStateToProps, mapDispatchToProps) (Users)
-
-
- 
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
